aae1c512b597eecf47781354dd1550f777f02718
Jan Skolimowski <100217193+jskol@users.noreply.github.com>
2024-03-11 10:17:53 +0100

    Update Haldane_Hamiltonian.hpp

--- Haldane_Hamiltonian.hpp
+++ Haldane_Hamiltonian.hpp
@@ -13,15 +13,15 @@ libcommute::expression<C, int, std::string> gen_Haldane_Hamiltonian(
     int num_of_sublattices{int(params.hopping.size())};
     for(auto spin :spins_set){
         for(int sub=0; sub< params.num_of_sites; sub += num_of_sublattices) {
-            H += 2.*params.hopping[1]*sin(2.*params.k)*c_dag(sub,spin)*c(sub,spin);
-            H += 2.*params.hopping[1]*sin(2.*params.k)*c_dag(sub+1,spin)*c(sub+1,spin);
+            H += -2.*params.hopping[1]*sin(2.*params.k)*c_dag(sub,spin)*c(sub,spin);
+            H += -2.*params.hopping[1]*sin(2.*params.k)*c_dag(sub+1,spin)*c(sub+1,spin);
             
             H += 2.*params.hopping[0]*cos(params.k)*c_dag(sub,spin)*c(sub+1,spin);
             H += 2.*params.hopping[0]*cos(params.k)*c_dag(sub+1,spin)*c(sub,spin);
             
             if(sub < params.num_of_sites-num_of_sublattices){
-                H +=  -2.*params.hopping[1]*sin(params.k)*(c_dag(sub,spin)*c(sub+num_of_sublattices,spin)+c_dag(sub+num_of_sublattices,spin)*c(sub,spin));        
-                H +=  -2.*params.hopping[1]*sin(params.k)*(c_dag(sub+1,spin)*c(sub+1+num_of_sublattices,spin)+c_dag(sub+1+num_of_sublattices,spin)*c(sub+1,spin));          
+                H +=  2.*params.hopping[1]*sin(params.k)*(c_dag(sub,spin)*c(sub+num_of_sublattices,spin)+c_dag(sub+num_of_sublattices,spin)*c(sub,spin));        
+                H +=  2.*params.hopping[1]*sin(params.k)*(c_dag(sub+1,spin)*c(sub+1+num_of_sublattices,spin)+c_dag(sub+1+num_of_sublattices,spin)*c(sub+1,spin));          
                 H +=  params.hopping[0]*(c_dag(sub+1,spin)*c(sub+num_of_sublattices,spin) +c_dag(sub+num_of_sublattices,spin)*c(sub+1,spin));
             }
         }
